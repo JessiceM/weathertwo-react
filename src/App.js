b778a53bb0829ba7dbcd079e6ca4f21368d130dd
JessiceM <jessica52mdz@gmail.com>
2022-11-02 22:45:30 -0500

    Added search box and submit button

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ export default function App() {
   return (
     <div className="App">
       Weather App
+
+      <form>
+        <input type="search" placeholder="Enter a city" className="form-control"/>
+        <input type="submit" value="Search" className="btn btn-primary" />
+      </form>
+<h1>Dallas</h1>
 <footer>
   This project was coded by<a href="" target="_blank"> Jessica Mendoza </a>  and is 
 <a href="https://github.com/JessiceM/weathertwo-react" target="_blank"> open-sourced on GitHub </a>
