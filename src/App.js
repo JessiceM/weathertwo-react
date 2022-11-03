4642cac9d20bd6fc8a58369f4570277cfa414ee3
JessiceM <jessica52mdz@gmail.com>
2022-11-02 22:26:08 -0500

    Add GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ export default function App() {
   return (
     <div className="App">
       Weather App
+<footer>
+  This project was coded by<a href="" target="_blank"> Jessica Mendoza </a>  and is 
+<a href="https://github.com/JessiceM/weathertwo-react" target="_blank"> open-sourced on GitHub </a>
+ and hosted on Netlify
+</footer>
+      
     </div>
   );
 }
