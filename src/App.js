7776b83f8a40a3469fc13aa4bb3deafd4c1f56ea
JessiceM <jessica52mdz@gmail.com>
2022-11-03 22:09:30 -0500

    Add Weather.js and .css

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React from "react";
+import Weather from "./Weather";
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-
-      <form>
+        <Weather />
+        <form>
         <input type="search" placeholder="Enter a city" className="form-control"/>
         <input type="submit" value="Search" className="btn btn-primary" />
-      </form>
-<h1>Dallas</h1>
-<footer>
-  This project was coded by<a href="" target="_blank"> Jessica Mendoza </a>  and is 
-<a href="https://github.com/JessiceM/weathertwo-react" target="_blank"> open-sourced on GitHub </a>
- and hosted on Netlify
-</footer>
+        </form>
+        <h1>Dallas</h1>
+        <footer>
+          This project was coded by<a href="" target="_blank"> Jessica Mendoza </a>  and is 
+            <a href="https://github.com/JessiceM/weathertwo-react" target="_blank"> open-sourced on GitHub </a>
+          and hosted on Netlify
+        </footer>
       </div>
     </div>
   );
