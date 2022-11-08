25cbb79cd1c6dc020ef3b3c2790e321440f31d36
JessiceM <jessica52mdz@gmail.com>
2022-11-07 22:41:31 -0600

    Changed positions inside row and city
    Used position: relative to move items around

--- src/Weather.js
+++ src/Weather.js
@@ -4,17 +4,17 @@ import "./Weather.css";
 export default function Weather() {
     return (
         <div className="Weather">
-            <div className="row">
-                
-                <div className="col-6">
+        
+            <div className="row ">
+                <div className="col-7">
                     <img src="http://openweathermap.org/img/wn/02d@2x.png" alt="Mostly Cloudy" />
                     <div className="condition">Cloudy Sky</div>
                 </div>
-                <div className="col-6">
+                <div className="col-4 align-self-center">
                     <h1><span className="temperature">15</span><span className="unit">°c</span></h1>
                     <ul>
                         <li>💧Humidity %</li>
-                        <li>💨Wind mph</li>
+                        <li>💨Wind mph</li> 
                     </ul>
                 </div>
             </div>
